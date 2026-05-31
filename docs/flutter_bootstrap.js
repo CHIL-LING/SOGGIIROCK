890a8c729531e3eb9795e4fabf7e27d8cf2b16a3
CHIL-LING <64gbmm5zwh@privaterelay.appleid.com>
2026-05-31 20:31:59 +0900

    업데이트

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1236995531" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2271554763" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
